21ad8e9b2dd43a12de8e047cd49098ee66c4c05a
Arunjegan <127992705+Jeganpetchimuthu@users.noreply.github.com>
2023-10-10 22:03:24 +0530

    Add files via upload
    React Day5

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+  {
+    id: 1,
+    name: "jegan",
+    username: "jegan1996",
+  },
+  {
+    id: 2,
+    name: "arun",
+    username: "arunkumar1998",
+  },
+];
+
+export default userList;
